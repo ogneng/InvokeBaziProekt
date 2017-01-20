def1041f74ab306960c1c379d552dccc267721a4
Igor-Nikolov <igor.nikoloov@gmail.com>
2017-01-21 00:28:24 +0100

    Connecting to non local Database,using ClientHelperClass

--- WebApiInvoke/Global.asax.cs
+++ WebApiInvoke/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebApiInvoke.Models;
 
 namespace WebApiInvoke
 {
@@ -18,6 +19,7 @@ namespace WebApiInvoke
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ClientHelperClass.connect();
         }
     }
 }
